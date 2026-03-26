e0a01b5b2200d050674fac598dd0afb909b49dc1
paripurna <paripurnatiwarigmail.com>
2026-03-26 14:52:13 +0530

    test_code_added

--- code.py
+++ code.py
@@ -1,21 +1,71 @@
 import hashlib
+import tkinter as tk
+from tkinter import filedialog
+
+
+bg_color = "#0D0630"
+btn_color = "#384E77"
+text_color = "#E6F9AF"
+
+file_path = ""
+original_hash = ""
 
 def get_hash(filename):
     with open(filename, "rb") as f:
-        data = f.read()
-        return hashlib.sha256(data).hexdigest()
+        return hashlib.sha256(f.read()).hexdigest()
 
-file = input("Enter file name: ")
+def select_file():
+    global file_path, original_hash
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        original_hash = get_hash(file_path)
+        file_label.config(text="File Selected", fg="lightgreen")
 
-hash1 = get_hash(file)
-print("Original Hash:", hash1)
+def check_file():
+    if not file_path:
+        result_label.config(text="Select file first!", fg="red")
+        return
 
-input("Modify the file and press Enter...")
+    new_hash = get_hash(file_path)
 
-hash2 = get_hash(file)
-print("New Hash:", hash2)
+    if new_hash == original_hash:
+        result_label.config(text="✔ File NOT changed", fg="lightgreen")
+    else:
+        result_label.config(text="✖ File MODIFIED", fg="red")
 
-if hash1 == hash2:
-    print("File is NOT changed")
-else:
-    print("File has been MODIFIED")
+root = tk.Tk()
+root.title("File Integrity Checker")
+root.geometry("500x300")
+root.config(bg=bg_color)
+
+title = tk.Label(root, text="File Integrity Checker",
+                 bg=bg_color, fg=text_color,
+                 font=("Arial", 18, "bold"))
+title.pack(pady=15)
+
+select_btn = tk.Button(root, text="Select File",
+                       command=select_file,
+                       bg=btn_color, fg="white",
+                       font=("Arial", 12), padx=10, pady=5)
+select_btn.pack(pady=10)
+
+
+file_label = tk.Label(root, text="No file selected",
+                      bg=bg_color, fg="white",
+                      font=("Arial", 10))
+file_label.pack()
+
+
+check_btn = tk.Button(root, text="Check Integrity",
+                      command=check_file,
+                      bg=btn_color, fg="white",
+                      font=("Arial", 12), padx=10, pady=5)
+check_btn.pack(pady=15)
+
+
+result_label = tk.Label(root, text="",
+                        bg=bg_color,
+                        font=("Arial", 14, "bold"))
+result_label.pack(pady=10)
+
+root.mainloop()
